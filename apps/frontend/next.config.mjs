e9f1f3a1a49b885286e92da582e5d420fbe08e13
shreyan01 <shreyanshukla19@gmail.com>
2024-09-18 14:58:15 +0530

    deployment changes

--- apps/frontend/next.config.mjs
+++ apps/frontend/next.config.mjs
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  reactStrictMode: true,
 }
 
 module.exports = nextConfig
